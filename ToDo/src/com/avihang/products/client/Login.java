6da272809a0a3f1ee31c76ac392d78212038844a
hadilenin <hadiyusefi98@gmail.com>
2022-11-28 15:41:15 +0330

    appended more components on login page

--- ToDo/src/com/avihang/products/client/Login.java
+++ ToDo/src/com/avihang/products/client/Login.java
@@ -1,33 +1,92 @@
 package com.avihang.products.client;
 
+import com.avihang.frameworks.ags.client.components.AMDBC_30_Action_Button;
+import com.avihang.frameworks.ags.client.components.AMDBC_31_Action_FloatingButton;
 import com.avihang.frameworks.ags.client.components.AMDBC_40_Form_TextBox;
 import com.avihang.frameworks.ags.client.components.AMDBC_41_Form_PWTextBox;
+import com.avihang.frameworks.ags.client.components.AMDBC_90_Card_Card;
+import com.avihang.frameworks.ags.client.components.AMDBC_92_Card_Header;
+import com.avihang.frameworks.ags.client.components.AMDBC_Enum.ButtonStyle;
+import com.google.gwt.dom.client.Style.Cursor;
+import com.google.gwt.dom.client.Style.FontStyle;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.i18n.client.HasDirection.Direction;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Login extends VerticalPanel{
 
-	private AMDBC_40_Form_TextBox username;
-	private AMDBC_41_Form_PWTextBox password;
+	private AMDBC_40_Form_TextBox 	tbUsername      = new AMDBC_40_Form_TextBox(DOM.createUniqueId(),"username", "your username here!") {
+
+		@Override
+		public void doEnterPressHandler() { 
+			// TODO Auto-generated method stub
+			Window.alert(tbUsername.getText());
+		}
+		
+		
+	};
+	private AMDBC_41_Form_PWTextBox pwPassword      = new AMDBC_41_Form_PWTextBox(DOM.createUniqueId(), "password", "your password here!") {
+
+		@Override
+		public void doEnterPressHandler() {
+			// TODO Auto-generated method stub
+			Window.alert(pwPassword.getValue());
+		}
+	};
+	
+	
+	private AMDBC_90_Card_Card             cdContainer     = new AMDBC_90_Card_Card(new AMDBC_92_Card_Header("Login", "text-center", "black-text", "info-color"),false);
+	private AMDBC_30_Action_Button         bnSubmit        = new AMDBC_30_Action_Button("sumbit",ButtonStyle.ADDITIONAL_CYAN,false,"");
+	private AMDBC_31_Action_FloatingButton fltBnRefresh    = new AMDBC_31_Action_FloatingButton("mdi-sync",ButtonStyle.ADDITIONAL_CYAN);
+	
 	
 	
 	public Login() {
-		pushWidgetsCenter();
+		pushWidgetsCenter(); 					// pushes Widgets to center 
 		
-		username = textBoxGetInstance();
-		password = PWTextGetInstance();
+		tbUsername.getElement().getStyle().setPropertyPx("width", 300);
+		tbUsername.getMainWidget().getElement().getStyle().setColor("black");
+		tbUsername.getElement().getStyle().setFontSize(15, Unit.PT);
+		pwPassword.getElement().getStyle().setPropertyPx("width", 300);
+		cdContainer.getElement().getStyle().setProperty("marginTop", "130px");
+		cdContainer.getElement().getStyle().setBackgroundColor("#86d1aa");
+		cdContainer.getElement().getStyle().setMarginRight(400, Unit.PX);
+		cdContainer.getElement().getStyle().setMarginLeft(400, Unit.PX);
+		cdContainer.getElement().getStyle().setFontStyle(FontStyle.ITALIC);
+		fltBnRefresh.getElement().getStyle().setCursor(Cursor.POINTER);
 		
-		username.getElement().getStyle().setPropertyPx("width", 300);
-		password.getElement().getStyle().setPropertyPx("width", 300);
 		
-		username.getMainWidget().getElement().getStyle().setProperty("marginTop", "200px");
+		bnSubmit.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent arg) {
+				// TODO Auto-generated method stub
+				Window.alert(tbUsername.getText() + "@" + pwPassword.getValue());
+			}
+		});
+		fltBnRefresh.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent arg0) {
+				// TODO Auto-generated method stub
+				tbUsername.setText("");
+				pwPassword.setValue("");
+			}
+		});
 		
-		add(username);
-		add(password);
+		cdContainer.addToContent(tbUsername);
+		cdContainer.addToContent(pwPassword);
+		cdContainer.addToContent(bnSubmit);
+		
+		add(fltBnRefresh);
+		add(cdContainer);
 	}
 	
 	
@@ -55,13 +114,13 @@ public class Login extends VerticalPanel{
 	}
 */
 	
-	private AMDBC_40_Form_TextBox textBoxGetInstance() {
+/*	private AMDBC_40_Form_TextBox textBoxGetInstance() {
 		return  new AMDBC_40_Form_TextBox(DOM.createUniqueId(),"username :", "your username here!") {
 
 			@Override
 			public void doEnterPressHandler() { 
 				// TODO Auto-generated method stub
-				Window.alert(username.getText());
+				Window.alert(tbUsername.getText());
 			}
 			
 			
@@ -74,11 +133,12 @@ public class Login extends VerticalPanel{
 			@Override
 			public void doEnterPressHandler() {
 				// TODO Auto-generated method stub
-				Window.alert(password.getValue());
+				Window.alert(pwPassword.getValue());
 			}
 		};
 	}
 	
+	*/
 }
 
 
