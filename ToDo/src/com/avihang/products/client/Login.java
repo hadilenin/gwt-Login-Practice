57ded31b31c55d825f96f963d9f032d8049d93b4
hadilenin <hadiyusefi98@gmail.com>
2022-11-29 17:58:55 +0330

    styled a gwt button from scratch

--- ToDo/src/com/avihang/products/client/Login.java
+++ ToDo/src/com/avihang/products/client/Login.java
@@ -1,12 +1,17 @@
 package com.avihang.products.client;
 
 import com.avihang.frameworks.ags.client.components.AMDBC_30_Action_Button;
+import com.google.gwt.user.client.Element;
 import com.avihang.frameworks.ags.client.components.AMDBC_31_Action_FloatingButton;
 import com.avihang.frameworks.ags.client.components.AMDBC_40_Form_TextBox;
 import com.avihang.frameworks.ags.client.components.AMDBC_41_Form_PWTextBox;
+import com.avihang.frameworks.ags.client.components.AMDBC_58_Compound_Captcha;
 import com.avihang.frameworks.ags.client.components.AMDBC_90_Card_Card;
 import com.avihang.frameworks.ags.client.components.AMDBC_92_Card_Header;
 import com.avihang.frameworks.ags.client.components.AMDBC_Enum.ButtonStyle;
+import com.avihang.frameworks.ags.client.components.AMDBC_Enum.SpinnerStyle;
+import com.avihang.frameworks.ags.client.components.AMDBC_Enum.TextStyle;
+import com.google.gwt.core.shared.GWT;
 import com.google.gwt.dom.client.Style.Cursor;
 import com.google.gwt.dom.client.Style.FontStyle;
 import com.google.gwt.dom.client.Style.Unit;
@@ -21,41 +26,46 @@ import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.i18n.client.HasDirection.Direction;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 public class Login extends VerticalPanel{
-
-	private AMDBC_40_Form_TextBox 	tbUsername      = new AMDBC_40_Form_TextBox(DOM.createUniqueId(),"username", "your username here!") {
+	
+	
+	private AMDBC_90_Card_Card             cdContainer     = new AMDBC_90_Card_Card(new AMDBC_92_Card_Header("Login", "text-center", "black-text", "info-color",null,false,"headerFont"),false);
+	private AMDBC_30_Action_Button         bnSubmit        = new AMDBC_30_Action_Button("sumbit",ButtonStyle.ADDITIONAL_CYAN,false,"");
+	private AMDBC_31_Action_FloatingButton fltBnRefresh    = new AMDBC_31_Action_FloatingButton("mdi-sync",ButtonStyle.ADDITIONAL_CYAN);
+	private AMDBC_40_Form_TextBox 	       tbUsername      = new AMDBC_40_Form_TextBox(DOM.createUniqueId(),"username", "your username here!") {
 
 		@Override
 		public void doEnterPressHandler() { 
-			// TODO Auto-generated method stub
 			Window.alert(tbUsername.getText());
 		}
 		
 		
 	};
-	private AMDBC_41_Form_PWTextBox pwPassword      = new AMDBC_41_Form_PWTextBox(DOM.createUniqueId(), "password", "your password here!") {
+	private AMDBC_41_Form_PWTextBox pwPassword     = new AMDBC_41_Form_PWTextBox(DOM.createUniqueId(), "password", "your password here!") {
 
 		@Override
 		public void doEnterPressHandler() {
-			// TODO Auto-generated method stub
 			Window.alert(pwPassword.getValue());
 		}
 	};
 	
-	
-	private AMDBC_90_Card_Card             cdContainer     = new AMDBC_90_Card_Card(new AMDBC_92_Card_Header("Login", "text-center", "black-text", "info-color"),false);
-	private AMDBC_30_Action_Button         bnSubmit        = new AMDBC_30_Action_Button("sumbit",ButtonStyle.ADDITIONAL_CYAN,false,"");
-	private AMDBC_31_Action_FloatingButton fltBnRefresh    = new AMDBC_31_Action_FloatingButton("mdi-sync",ButtonStyle.ADDITIONAL_CYAN);
-	
+
 	
 	
 	public Login() {
-		pushWidgetsCenter(); 					// pushes Widgets to center 
+		pushWidgetsCenter(); 		// pushes Widgets to center 
+		
+	    Button button = new Button("temp");
+		button.addStyleName("buttonElement button1");
+		
 		
 		tbUsername.getElement().getStyle().setPropertyPx("width", 300);
 		tbUsername.getMainWidget().getElement().getStyle().setColor("black");
@@ -67,11 +77,12 @@ public class Login extends VerticalPanel{
 		cdContainer.getElement().getStyle().setFontStyle(FontStyle.ITALIC);
 		fltBnRefresh.getElement().getStyle().setCursor(Cursor.POINTER);
 		
+		
 		bnSubmit.addClickHandler(new ClickHandler() {
 			
 			@Override
 			public void onClick(ClickEvent arg) {
-				
+			
 				if (pwPassword.getValue().isEmpty()) 
 					pwPassword.showError("password input was glossed over!");
 				
@@ -110,7 +121,8 @@ public class Login extends VerticalPanel{
 		cdContainer.addToContent(tbUsername);
 		cdContainer.addToContent(pwPassword);
 		cdContainer.addToContent(bnSubmit);
-		
+		cdContainer.addToContent(button);
+
 		add(fltBnRefresh);
 		add(cdContainer);
 	}
--- ToDo/src/com/avihang/products/client/TikTakToe.java
+++ ToDo/src/com/avihang/products/client/TikTakToe.java
@@ -7,31 +7,50 @@ import com.avihang.frameworks.ags.client.components.AMDBC_31_Action_FloatingButt
 import com.avihang.frameworks.ags.client.components.AMDBC_31_Action_IconButton;
 import com.avihang.frameworks.ags.client.components.AMDBC_90_Card_Card;
 import com.avihang.frameworks.ags.client.components.AMDBC_Enum.ButtonStyle;
+import com.gargoylesoftware.htmlunit.WebConsole.Logger;
+import com.google.gwt.dom.client.Style.Display;
 import com.google.gwt.dom.client.Style.Position;
+import com.google.gwt.dom.client.Style.TextJustify;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.resources.css.InterfaceGenerator;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.FlowPanel;
 
 public class TikTakToe extends FlowPanel {
 
-	private AMDBC_90_Card_Card                 cdQuarterContainer         = new AMDBC_90_Card_Card("TikTakToe","yoo hoo","",false);
+	private AMDBC_90_Card_Card                 cdQuarterContainer         = new AMDBC_90_Card_Card("","","",false);
 	private ArrayList<AMDBC_30_Action_Button>  quarterList  			  = new ArrayList<>();
+	
+	private TurnBase currentTurn = TurnBase.O;
+	private boolean[] wasClicked = new boolean[9]; 
 	
 	public TikTakToe() {
 		
 		for(int i = 0;i < 9;i++) {
 			quarterList.add(new AMDBC_30_Action_Button(" ",ButtonStyle.ADDITIONAL_CYAN,false,""));
 			quarterList.get(i).getElement().getStyle().setWidth(100, Unit.PX);
-			quarterList.get(i).setLargeButton();
+			
+			quarterList.get(i).setText(" ");
+			int j = i;
+			quarterList.get(i).addClickHandler(new ClickHandler() {
+				
+				@Override
+				public void onClick(ClickEvent arg0) {
+					
+					if(!wasClicked[j]) {
+						if(nextTurn() == TurnBase.O)
+							quarterList.get(j).setText("O");
+						else 
+							quarterList.get(j).setText("X");
+						wasClicked[j] = true;
+					}
+				}
+			});
 		}
 		
-		quarterList.get(0).getElement().getStyle().setPosition(Position.RELATIVE);
-		quarterList.get(0).getElement().getStyle().setRight(100, Unit.PX);
-		quarterList.get(0).getElement().getStyle().setTop(50, Unit.PX);
-		
-		//positionQuarters();
+		positionQuarters();
 	
 		cdQuarterContainer.getElement().getStyle().setMargin(300, Unit.PX);
 	
@@ -40,19 +59,26 @@ public class TikTakToe extends FlowPanel {
 			cdQuarterContainer.add(quarterList.get(i));
 		
 		add(cdQuarterContainer);
+		
+		
 	}
 	
 	private void positionQuarters() {
-		int marginRightPx = 500;
-		int liftPx = 50;
-		int shiftRightPx = formatter(quarterList.get(0).getElement().getStyle().getWidth());
+		int positionRightPx = 455;
+		int liftPx = 45;
+		int shiftRightPx = formatter(quarterList.get(0).getElement().getStyle().getWidth()) + 30;
 		int j = 0;
 		while(j == 0 || j == 3 || j == 6) {
+			quarterList.get(j).getElement().getStyle().setPosition(Position.RELATIVE);
+			quarterList.get(j + 1).getElement().getStyle().setPosition(Position.RELATIVE);
+			quarterList.get(j + 2).getElement().getStyle().setPosition(Position.RELATIVE);
 			
-			quarterList.get(j).getElement().getStyle().setMarginRight(marginRightPx, Unit.PX);
-			quarterList.get(j + 1).getElement().getStyle().setMarginRight(marginRightPx - 50, Unit.PX);
-			quarterList.get(j + 1).getElement().getStyle().setMarginBottom(liftPx, Unit.PX);
-			quarterList.get(j + 2).getElement().getStyle().setMarginRight(marginRightPx, Unit.PX);
+			quarterList.get(j).getElement().getStyle().setRight(positionRightPx, Unit.PX);
+			quarterList.get(j + 1).getElement().getStyle().setRight(positionRightPx - shiftRightPx, Unit.PX);
+			quarterList.get(j + 2).getElement().getStyle().setRight(positionRightPx - (2 * shiftRightPx), Unit.PX);
+			
+			quarterList.get(j + 1).getElement().getStyle().setBottom(liftPx, Unit.PX);
+			quarterList.get(j + 2).getElement().getStyle().setBottom(2 * liftPx, Unit.PX);
 			
 			j += 3;
 		}
@@ -63,4 +89,22 @@ public class TikTakToe extends FlowPanel {
 		return Integer.parseInt(str);
 	}
 
+	private TurnBase nextTurn() {
+		switch (currentTurn) {
+		case X:
+			currentTurn = TurnBase.O;
+			break;
+		case O:
+			currentTurn = TurnBase.X;
+			break;
+		}
+		
+		return currentTurn;
+	}
+	
+
+	enum TurnBase{
+		X,O
+	}
+	
 }
